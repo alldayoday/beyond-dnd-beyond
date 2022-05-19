36cb5ece9e7e6cd1572007382559606bc27afcb7
Erin ODea <odea.erin@gmail.com>
2022-05-19 14:05:11 -0500

    cleanup

--- routes/characters.js
+++ routes/characters.js
@@ -8,15 +8,20 @@ const router = Router()
 //CRUD for character control
 router.get('/', charactersCtrl.index)
 router.get('/new', isLoggedIn, passUserToView, charactersCtrl.new)
+
 router.post('/', isLoggedIn, passUserToView, charactersCtrl.create)
+
 router.get("/:id", isLoggedIn, passUserToView, charactersCtrl.show)
 router.get("/:id/edit", isLoggedIn, passUserToView, charactersCtrl.edit)
+
 router.put("/:id", isLoggedIn, passUserToView, charactersCtrl.update)
+
 router.delete("/:id", isLoggedIn, passUserToView, charactersCtrl.delete)
 
 //adding and removing spells and weapons from embedded models
 router.post('/:id/spells', isLoggedIn, passUserToView, charactersCtrl.createSpell)
 router.post('/:id/weapons', isLoggedIn, passUserToView, charactersCtrl.equipWeapon)
+
 router.delete("/:charId/spells/:spellId", isLoggedIn, passUserToView, charactersCtrl.deleteSpell)
 router.delete("/:charId/weapons/:weaponId", isLoggedIn, passUserToView, charactersCtrl.deleteWeapon)
 
